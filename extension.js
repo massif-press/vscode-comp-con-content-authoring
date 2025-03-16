7c138bb52244d3e5a610d7bec3a303a2a0a149e4
John A <john.arena.3@gmail.com>
2025-03-16 12:25:21 -0500

    stage automation

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 'use strict';
 const vscode = require('vscode');
-const os = require('os')
-const path = require('path')
+const os = require('os');
+const path = require('path');
 
 const { exists } = require('fs');
 const { promisify } = require('util');
@@ -9,75 +9,76 @@ const { promisify } = require('util');
 const existsPromise = promisify(exists);
 
 const files = [
-	"lcp_manifest.json",
-	"manufacturers.json",
-	"core_bonuses.json",
-	"environments.json",
-	"frames.json",
-	"weapons.json",
-	"systems.json",
-	"mods.json",
-	"pilot_gear.json",
-	"reserves.json",
-	"sitreps.json",
-	"skills.json",
-	"statuses.json",
-	"talents.json",
-	"tags.json",
-	"npc_classes.json",
-	"npc_features.json",
-	"npc_templates.json",
-]
+  'lcp_manifest.json',
+  'manufacturers.json',
+  'core_bonuses.json',
+  'environments.json',
+  'frames.json',
+  'weapons.json',
+  'systems.json',
+  'mods.json',
+  'pilot_gear.json',
+  'reserves.json',
+  'sitreps.json',
+  'skills.json',
+  'statuses.json',
+  'talents.json',
+  'tags.json',
+  'npc_classes.json',
+  'npc_features.json',
+  'npc_templates.json',
+];
 
 function activate() {
+  var type = 'compconTaskProvider';
+  vscode.tasks.registerTaskProvider(type, {
+    async provideTasks() {
+      const root = vscode.workspace.workspaceFolders[0].uri.fsPath;
 
-	var type = "compconTaskProvider";
-	vscode.tasks.registerTaskProvider(type, {
-		async provideTasks() {
+      const manifestExists = await existsPromise(root + '/lcp_manifest.json');
 
-			const root = vscode.workspace.workspaceFolders[0].uri.fsPath
+      if (!manifestExists) return [];
 
-			const manifestExists = await existsPromise(root + '/lcp_manifest.json')
+      const textDoc = await vscode.workspace.openTextDocument(root + '/lcp_manifest.json');
 
-			if (!manifestExists) return []
+      const existingFiles = (
+        await Promise.all(
+          files.map(async (filename) => {
+            const doesExist = await existsPromise(path.resolve(root, filename));
+            return doesExist ? filename : null;
+          })
+        )
+      ).filter((x) => x);
 
-			const textDoc = await vscode.workspace.openTextDocument(root + '/lcp_manifest.json')
+      const manifest = JSON.parse(textDoc.getText());
+      const { name, version } = manifest;
 
+      const cmd = os.platform() === 'win32' ? '7z a -tzip' : 'zip';
+      const packageName = `${name}-${version}.lcp`;
+      const filesStr = existingFiles.join(' ');
 
+      const task = new vscode.Task(
+        { type },
+        'Build .LCP package',
+        'compcon',
+        new vscode.ShellExecution(`${cmd} "${packageName.replace('"', '\\"')}" ${filesStr}`),
+        []
+      );
 
-			const existingFiles = (await Promise.all(files.map(async (filename) => {
-				const doesExist = await existsPromise(path.resolve(root, filename))
-				return doesExist ? filename : null
-			}))).filter(x => x)
+      task.group = vscode.TaskGroup.Build;
 
-
-			const manifest = JSON.parse(textDoc.getText())
-			const { name, version } = manifest
-
-			const cmd = os.platform() === 'win32' ? '7z a -tzip' : 'zip'
-			const packageName = `${name}-${version}.lcp`
-			const filesStr = existingFiles.join(' ')
-
-			const task = new vscode.Task({ type }, 'Build .LCP package', 'compcon',
-				new vscode.ShellExecution(
-					`${cmd} "${packageName.replace('\"', '\\\"')}" ${filesStr}`
-				), []);
-
-			task.group = vscode.TaskGroup.Build
-
-			return [task]
-		},
-		resolveTask(task) {
-			return task;
-		}
-	});
+      return [task];
+    },
+    resolveTask(task) {
+      return task;
+    },
+  });
 }
-exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
